c984fed589694cb898b7d5800579949e1340a8d5
jacob-gilbert <jacob.gilbert@richmond.edu>
2025-07-19 10:25:22 -0400

    added comments

--- cli_calculator.py
+++ cli_calculator.py
@@ -15,12 +15,17 @@ class CLI_Calculator:
         return num1 / num2
 
 
+# if user wants to use the calculator create a calculator instance to use
 use_calc = input("Do you want to use the calculator? (y/n)\n")
 if use_calc:
     calculator = CLI_Calculator()
-    
+   
+# using a while loop so we can continuously use the calculator even after it has been used 
 while use_calc:
+    # take in user input and decide if the user wants to continue using the calculator
     print("Calculator is ready to be used")
     line = input("Type a line of math into the calculator to continue or 'n' to close\n")
     if line.lower() == "n":
         break
+    
+    
