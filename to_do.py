884417794c088b3f29787d88f787fd94d6f93bac
jacob-gilbert <jacob.gilbert@richmond.edu>
2025-07-24 10:36:13 -0400

    updated delete for linked list

--- to_do.py
+++ to_do.py
@@ -45,36 +45,32 @@ while keep_going:
         priority = input("Enter the priority of your task where 1 is most important and 100 is least important\n")
         notes = input("Enter any notes you would like to include about the task\n")
         print("Adding task...")
-        temp_task = Task(name, priority, notes)
+        new_task = Task(name, priority, notes)
         
         # find proper location in linked list
-        if root == None: # linked list empty, make first task the root
-            root = temp_task
-        elif temp_task < root:
-            temp = root
-            temp_task.next = temp
-            root = temp_task
-        elif root.next == None:
-            root.next = temp_task
-        else: # more than two elements in linked list
+        if root.next == None: # linked list empty, add first task
+            root.next = new_task
+        else: # at least one task in linked list already
             curr = root.next
             prev = root
             while curr != None:
-                if temp_task < curr:
-                    prev.next = temp_task
-                    temp_task.next = curr
+                if new_task < curr:
+                    prev.next = new_task
+                    new_task.next = curr
                     break
+
+                # update pointers
                 prev = curr
                 curr = curr.next
                     
-                # if this is reached the while loop has ended without adding the new task
-                # put the task at the end of the linked list
-                prev.next = temp_task
+            # if this is reached the while loop has ended without adding the new task
+            # put the task at the end of the linked list
+            prev.next = new_task
 
     # view tasks    
     elif answer == "b":
         # check the number of tasks in the list, if its zero tell the user there are not tasks, otherwise print the list of tasks
-        if root == None:
+        if root.next == None:
             print("No tasks found")
             continue
             
@@ -88,7 +84,7 @@ while keep_going:
     # delete task
     elif answer == "c":
         # check if there are any
-        if root == None:
+        if root.next == None:
             print("No tasks found")
             continue
 
@@ -98,16 +94,16 @@ while keep_going:
         print("Finding task...")
         
         # iterate through task list to find tasks with the given name
-        curr = root
-        while curr.next != None:
-            temp_task = task_list[i]
-            if temp_task.get_name() == name:
-                print(f"Identified Task {name} with priority {temp_task.get_priority()} and notes {temp_task.get_notes()}")
+        curr = root.next
+        prev = root
+        while curr != None:
+            if curr.get_name() == name:
+                print(f"Identified Task {name} with priority {curr.get_priority()} and notes {curr.get_notes()}")
                 delete_task = input("Should this task be deleted? (y/n)\n")
                 
                 # task is set to be deleted so delete task and stop searching
                 if delete_task == "y":
-                    task_list.pop(i)
+                    prev.next = curr.next
                     task_deleted = True
                     break
                 
@@ -117,6 +113,9 @@ while keep_going:
                 else:
                     print("Incorrect input, task will not be deleted, restarting...")
                     break
+            
+            prev = curr
+            curr = curr.next
         
         # inform user whether or not their task was deleted            
         if not task_deleted:
